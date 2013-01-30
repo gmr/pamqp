8f9615ebd7ae4802b1e44d6b8243aafb785a7fa3
Gavin M. Roy <gmr@meetme.com>
2013-01-29 22:53:18 -0500

    Use absolute imports and fix the version string

--- pamqp/__init__.py
+++ pamqp/__init__.py
@@ -3,12 +3,12 @@
 __author__ = 'Gavin M. Roy'
 __email__ = 'gavinmroy@gmail.com'
 __since__ = '2011-09-23'
-__version__ '1.0.1'
+__version__ = '1.0.1'
 
-from header import ProtocolHeader
-from header import ContentHeader
+from pamqp.header import ProtocolHeader
+from pamqp.header import ContentHeader
 
-import body
-import codec
-import frame
-import specification
+from pamqp import body
+from pamqp import codec
+from pamqp import frame
+from pamqp import specification
