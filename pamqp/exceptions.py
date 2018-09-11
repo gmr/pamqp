f128fd5e955271600bb2a525d830b9d5abe019f0
Gavin M. Roy <gavinr@aweber.com>
2018-09-11 13:43:51 -0400

    Add base exception type (#4)

--- pamqp/exceptions.py
+++ pamqp/exceptions.py
@@ -1,11 +1,17 @@
-"""pamqp specific exceptions that do not fit within the AMQP exceptions
+# -*- encoding: utf-8 -*-
+"""
+pamqp specific exceptions that do not fit within the AMQP exceptions
 contained in pamqp.specification
 
 """
 
 
-class UnmarshalingException(Exception):
+class PAMQPException(Exception):
+    """Base exception for all pamqp specific exceptions"""
+
+
+class UnmarshalingException(PAMQPException):
     """Raised when a frame is not able to be unmarshaled."""
-    def __repr__(self):
-        return "Could not unmarshal %s frame: %s" % (self.args[0],
-                                                     self.args[1])
+    def __str__(self):
+        return 'Could not unmarshal {} frame: {}'.format(
+            self.args[0], self.args[1])
