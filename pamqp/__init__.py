0aab3a71ae765d7dd4a12bcb15b6e9d139a8118a
Gavin M. Roy <gavinr@aweber.com>
2018-09-11 13:49:19 -0400

    Bump version

--- pamqp/__init__.py
+++ pamqp/__init__.py
@@ -1,8 +1,9 @@
+# -*- encoding: utf-8 -*-
 """AMQP Specifications and Classes"""
 __author__ = 'Gavin M. Roy'
 __email__ = 'gavinmroy@gmail.com'
 __since__ = '2011-09-23'
-__version__ = '1.6.1'
+__version__ = '2.0.0'
 
 import sys
 
