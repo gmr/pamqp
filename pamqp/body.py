c5001c6f6dab2639fdeb5735f4d4f6f7b8d35395
Gavin M. Roy <gavinr@aweber.com>
2019-12-16 17:36:26 -0500

    Update to include typing, cleanup docstrings and code

--- pamqp/body.py
+++ pamqp/body.py
@@ -5,45 +5,31 @@ unmarshaling body frames. When dealing with content frames, the message body
 will be returned from the library as an instance of the body class.
 
 """
+import typing
 
 
-class ContentBody(object):
+class ContentBody:
     """ContentBody carries the value for an AMQP message body frame"""
-    name = 'ContentBody'
-
-    def __init__(self, value=None):
-        """Create a new instance of a ContentBody object, passing in the value
-        of the message body
-
-        :param str|unicode|bytes value: The content body
-
-        """
+    def __init__(self, value: typing.Optional[bytes] = None):
+        """Create a new instance of a ContentBody object"""
         self.value = value
 
-    def __len__(self):
-        """Return the length of the content body value
+    def __len__(self) -> int:
+        """Return the length of the content body value"""
+        return len(self.value) if self.value else 0
 
-        :rtype: int
-
-        """
-        return len(self.value)
-
-    def marshal(self):
+    def marshal(self) -> bytes:
         """Return the marshaled content body. This method is here for API
-        compatibility, there is no special marhsaling for the payload in a
+        compatibility, there is no special marshaling for the payload in a
         content frame.
-
-        :rtype: str|unicode|bytes
 
         """
         return self.value
 
-    def unmarshal(self, data):
+    def unmarshal(self, data: bytes) -> typing.NoReturn:
         """Apply the data to the object. This method is here for API
         compatibility, there is no special unmarhsaling for the payload in a
         content frame.
-
-        :rtype: str|unicode|bytes
 
         """
         self.value = data
