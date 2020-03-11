1a6ea5fefbd1a5a9532583e0bd73a5d771133a17
Gavin M. Roy <gavinr@aweber.com>
2020-03-11 17:02:00 -0400

    Remove old CI scripts

--- bin/upload-coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-if [ "${TRAVIS_REPO_SLUG}" = "gmr/pamqp" ];
-then
-    echo "Uploading coverage for ${TRAVIS_PYTHON_VERSION} to S3"
-    aws s3 cp .coverage "s3://com-gavinroy-travis/pamqp/$TRAVIS_BUILD_NUMBER/.coverage.${TRAVIS_PYTHON_VERSION}"
-fi
